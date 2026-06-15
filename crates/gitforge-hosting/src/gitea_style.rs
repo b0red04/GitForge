23e3f27ae05ecd64666ea1ac7c6b3cd6fbab60df
Jason West <131637203+b0red04@users.noreply.github.com>
2026-06-14 21:00:17 -0400

    Fix repo search URL missing page_size parameter value

--- crates/gitforge-hosting/src/gitea_style.rs
+++ crates/gitforge-hosting/src/gitea_style.rs
@@ -211,11 +211,12 @@ impl HostingProvider for GiteaStyleProvider {
         let client = self.make_client(&token);
         let response = client
             .get(format!(
-                "{}{}?q={}&{}=30&sort=updated",
+                "{}{}?q={}&{}={}&sort=updated",
                 self.base_url,
                 self.config.search_repos_path,
                 http::url_encode_query(query),
-                self.config.page_param
+                self.config.page_param,
+                self.config.page_size
             ))
             .send()
             .await?;
